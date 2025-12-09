41d0ae7b7794c52feb8e921fda64974ca3e99b5e
ymohamud4 <124330343+ymohamud4@users.noreply.github.com>
2025-12-09 12:17:05 -0500

    Update interface files

---
+++ src/MoviePlaylistKernel.java
@@ -0,0 +1,70 @@
+/**
+ * Kernel interface for a Movie Playlist.
+ *
+ * these are the kernel for {@code MusicPlayListKernel} which extends from
+ * {@code Standard}
+ *
+ * @author Yonis Mohamud
+ *
+ *         this project is cool
+ *
+ */
+
+public interface MoviePlaylistKernel extends Standard<MoviePlaylistKernel> {
+
+    /*
+     *
+     * @param movieTitle the name of the movieTitle you will add
+     *
+     * @updates addMovie = #this with movieTitle Added
+     *
+     *
+     */
+
+    
+    void addMovie(String movieTitle);
+
+    /*
+     *
+     * @param movieTitle the name of the movie you will remove
+     *
+     * @updates #this = #this with the specifc movieTitle removed
+     *
+     * @Requires #this = contains movieTitle
+     *
+     * @ensure eraseMovie = #this with specific movieTitle removed
+     *
+     */
+
+    void eraseMovie(String movieTitle);
+
+    /*
+     *
+     * @return returns the amount of movies in the playlist #this
+     *
+     */
+
+    int size();
+
+    /*
+     *
+     * @Param movieTitle movie to insert
+     *
+     * @update this
+     *
+     * @requires this.size >0
+     *
+     * @ensure this = at least one movie title has been replace
+     *
+     */
+
+    void replaceMovie(String movieTitle);
+
+    /**
+     * Removes and returns any movie from the playlist.
+     *
+     * @updates this
+     * @ensures removeAny is in #this
+     */
+    String removeAny();
+}
